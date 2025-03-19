e7b15254d66d0e2e995d564cb4d8cd0dca4ee302
joaoggoncalo <jppjgp@gmail.com>
2025-03-19 15:24:48 +0100

    deleting flutter related things

--- android/app/src/main/kotlin/com/example/party_planner_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.party_planner_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/utils/time_picker_util.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-Future<TimeOfDay?> selectTime(BuildContext context, TimeOfDay initialTime) async {
-  return await showTimePicker(
-    context: context,
-    initialTime: initialTime,
-  );
-}
